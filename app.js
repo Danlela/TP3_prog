eade7f334c8b65aff61362a11f153c7ff92d5be7
Danlela <117685047+Danlela@users.noreply.github.com>
2023-06-15 23:58:38 -0400

    test

--- app.js
+++ app.js
@@ -3,19 +3,9 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const cors = require('cors');
 
-// Middleware CORS
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  // res.setHeader('Access-Control-Allow-Origin', '');
-
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
-  );
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
+app.use(cors());
 
 
 // Importe les routes
